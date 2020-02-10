b7af1e8f2bb3c359def4dfa01002a1ff20fbc1ba
YovanJong <50098631+YovanJong@users.noreply.github.com>
2020-02-10 08:52:59 +0700

    [EDIT] Accordion
    Tambah Seluruh Jenis penyakit dan lebar dari accordion

--- mtbs-web/src/View/Summary/summary.js
+++ mtbs-web/src/View/Summary/summary.js
@@ -14,9 +14,39 @@ function App() {
       />
       <Accordion
         title="Diare"
-        content="
-        <p>Diare Dehidrasi Berat</p>
-        "
+        content="<p>Diare Dehidrasi Berat</p>"
+      />
+      <Accordion
+        title="Masalah Telinga"
+        content="<p>Telinga Sehat</p>"
+      />
+      <Accordion
+        title="Status Gizi"
+        content="<p>Gizi Sehat</p>"
+      />
+      <Accordion
+        title="Anemia"
+        content="<p>Anemia Berat</p>"
+      />
+      <Accordion
+        title="Status HIV"
+        content="<p>Terkena HIV</p>"
+      />
+      <Accordion
+        title="Status Imunisasi"
+        content="<p>Diberikan Polio 4 Hari Ini</p>"
+      />
+      <Accordion
+        title="Vitamin A"
+        content="<p>Vitamin A</p>"
+      />
+      <Accordion
+        title="Keluhan Lain"
+        content="<p>Sakit leher</p>"
+      />
+      <Accordion
+        title="Pemberian Makan"
+        content="<p>Sudah Baik</p>"
       />
     </div>
   );

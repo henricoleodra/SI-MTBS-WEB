57eb96f0af38f39f13e05d9333351d4bc733f4cc
friska christiana <christianafriska@gmail.com>
2020-04-24 18:30:34 +0700

    [ADD] Backend Batuk2, AnsBatuk
    belom batuk controllernya

--- mtbs-web/src/Reducers/AnsBatuk.js
+++ mtbs-web/src/Reducers/AnsBatuk.js
@@ -2,7 +2,9 @@ const initState = {
     bsb_lamaHari : null,
     bsb_jumlahNafas : null,
     bsb_nafasCepat : null,
-    bsb_tarikanDindingDada : null
+    bsb_tarikanDindingDada : null,
+    bsb_wheezing : null,
+    bsb_saturasiOksigen : null
 }
 
 const ansBatukReducer = (state = initState, action) => {
@@ -22,6 +24,14 @@ const ansBatukReducer = (state = initState, action) => {
         case 'TARIKAN_DINDING_DADA':
             return Object.assign({}, state, {
                 bsb_tarikanDindingDada : action.answer
+            });
+        case 'WHEEZING':
+            return Object.assign({}, state, {
+                bsb_wheezing : action.answer
+            });
+        case 'SATURASI_OKSIGEN':
+            return Object.assign({}, state, {
+                bsb_saturasiOksigen : action.answer
             });
         default:
             return state;

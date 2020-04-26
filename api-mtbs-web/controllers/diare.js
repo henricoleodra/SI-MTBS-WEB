7dff2a7f073a6a0c329caea37c88bca7e36bfd65
Yovannanta Jong <jongyovannanta@gmail.com>
2020-04-26 18:51:52 +0700

    [UPDATE] Backend Diare (Sidebar & Summary blm konek)

--- api-mtbs-web/controllers/diare.js
+++ api-mtbs-web/controllers/diare.js
@@ -45,6 +45,30 @@ const diare2 = async function(req, res, next) {
     }
 };
 const diare3 = async function(req, res, next) {
+    let berat = 0;
+    let ringan = 0;
+    if(req.body.diare_isAnakTidakSadar){
+        berat = berat + 1;
+    }
+    if(req.body.diare_rewelMudahMarah){
+        ringan = ringan + 1;
+    }
+    if(req.body.diare_isMataCekung){
+        berat = berat + 1;
+        ringan = ringan + 1;
+    }
+    if(req.body.diare_isNotMinum){
+        berat = berat + 1;
+    }
+    if(req.body.diare_isNotMinum == false){
+        ringan = ringan + 1;
+    }
+    if(req.body.diare_kulitPerutLambat === "SangatLambat"){
+        berat = berat + 1;
+    }
+    if(req.body.diare_kulitPerutLambat === "Lambat"){
+        ringan = ringan + 1;
+    }
     if(req.body.diare_tinjaBerdarah){
         const ans = {
             hasilKlasifkasi : 'Disentri',
@@ -67,6 +91,20 @@ const diare3 = async function(req, res, next) {
             }
             res.json(ans);
         }
+    }
+    else if(berat > 1){
+        const ans = {
+            hasilKlasifkasi : 'Diare Dehidrasi Berat',
+            statusKlasifikasi: 'danger',
+        }
+        res.json(ans);
+    }
+    else if(ringan > 1){
+        const ans = {
+            hasilKlasifkasi : 'Diare Dehidrasi Ringan/Sedang',
+            statusKlasifikasi: 'warning',
+        }
+        res.json(ans);
     }
     else{
         const ans = {

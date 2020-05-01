20be9d49698b95dbba10b24db5c8729a745c1f1f
NaomiShinna <myrichardkun1@gmail.com>
2020-05-01 23:51:21 +0700

    [UPDATE]Telinga di routes

--- api-mtbs-web/routes/index.js
+++ api-mtbs-web/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const tandabahayaumum = require('../controllers/tandabahayaumum');
 const batuk = require('../controllers/batuk');
 const diare = require('../controllers/diare');
+const telinga = require('../controllers/telinga');
 const anemia = require('../controllers/anemia');
 const hiv = require('../controllers/hiv');
 
@@ -21,6 +22,9 @@ router.post('/Batuk/2', batuk.batuk2);
 router.post(`/Diare/1`,diare.diare1);
 router.post(`/Diare/2`,diare.diare2);
 router.post(`/Diare/3`,diare.diare3);
+//Telinga
+router.post(`/Telinga/1`,telinga.telinga1);
+router.post(`/Telinga/2`,telinga.telinga2);
 //Anemia
 router.post(`/Anemia`,anemia.anemia);
 //HIV

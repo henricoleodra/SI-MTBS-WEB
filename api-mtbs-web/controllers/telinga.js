a8a7853315850849e1ea7ae856ad4d22ccd57d21
NaomiShinna <myrichardkun1@gmail.com>
2020-05-01 23:49:24 +0700

    [UPDATE] controller telinga

--- api-mtbs-web/controllers/telinga.js
+++ api-mtbs-web/controllers/telinga.js
@@ -1,19 +1,32 @@
 const klasifikasiTelinga = require('../models/telinga');
+const klasifikasiTelinga2 = require('../models/telinga');
 
-const telinga =async function(req, res, next){
-    const klasTelinga = await klasifikasiTelinga.klasifikasiTelinga(req.body.telinga);
+const telinga = async function(req, res, next){
     // kamu lihat front end di folder reducer, AnsTelinga
-    const klasTelinga = await klasifikasiTelinga.klasifikasiTelinga(req.body.telinga_isNyeri);
-    // penuh
-
-    // nanah
-    
-    // tambahin seluruh yang ada di case AnsTelinga 
-    
+    const klasTelinga = await klasifikasiTelinga.klasifikasiTelinga(req.body.telinga_isNyeri, 
+        req.body.telinga_isPenuh, 
+        req.body.telinga_isNanah, 
+        req.body.telinga_nanahLamaHari);
     
     const ans = {
         hasilKlasifikasi : klasTelinga.hasilKlasifikasi,
         statusKlasifikasi : klasTelinga.statusKlasifikasi,
     }
-    res.json(ans)
-}
+    res.json(ans);
+};
+
+const telinga2 = async function(req, res, next){
+    const klasTelinga = await klasifikasiTelinga2.klasifikasiTelinga(req.body.telinga_cekKeluarNanah, 
+        req.body.telinga_isBengkak);
+
+    const ans = {
+        hasilKlasifikasi : klasTelinga.hasilKlasifikasi,
+        statusKlasifikasi : klasTelinga.statusKlasifikasi,
+    }
+    res.json(ans);
+};
+
+module.exports = {
+    telinga,
+    telinga2
+};

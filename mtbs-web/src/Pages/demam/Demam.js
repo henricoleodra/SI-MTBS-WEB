4a2cdd3f794795ff6ee01a30fbcbc0d4bc6ae7e7
NaomiShinna <myrichardkun1@gmail.com>
2020-05-07 23:51:48 +0700

    [UPDATE] Memperbaiki radio button dan menambah command
    Command tersebut berisi template yang hanya perlu di modifikasi dan di un-command

--- mtbs-web/src/Pages/demam/Demam.js
+++ mtbs-web/src/Pages/demam/Demam.js
@@ -4,6 +4,7 @@ import { FormGroup, Label, Input, Form, Card, CardBody, CardTitle, Button, Row, 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircle, faChevronLeft, faChevronRight} from '@fortawesome/free-solid-svg-icons'
 
+import '../../Assets/style/style.css';
 
 var outlineColor = {
     borderColor: '#41E8B3'
@@ -11,7 +12,7 @@ var outlineColor = {
 
 const Demam = (props) => {
     return (
-        <Form>
+        <Form /**onSubmit={handleSubmit}**/>
             <div className="w-100">
                 <div className="col-12">
                     <div className="d-flex justify-content-center mt-3">
@@ -56,63 +57,87 @@ const Demam = (props) => {
                             }}
                         />
                     </div>
-                    <div style={{minHeight: "500px"}}>
+                    <div style={{minHeight: "475px"}}>
                         <Row className="justify-content-around">
                             <Card style={outlineColor} className="text-center w-75" >
                                 <CardBody>
                                     <CardTitle className="h5"><b>Tanyakan dan periksa!</b></CardTitle>
-                                    <FormGroup check className="d-inline pr-2">
-                                        <Label cek>
-                                            <Input type="radio" name="radio1" />{''}
-                                            Anamnesis
-                                        </Label>
-                                    </FormGroup>
-                                    <FormGroup check className="d-inline">
-                                        <Label cek>
-                                            <Input type="radio" name="radio1" />{''}
-                                            Teraba panas
-                                        </Label>
-                                    </FormGroup>
-                                    <FormGroup check className="d-inline pl-4">
-                                        <Label cek>
-                                            <Input type="radio" name="radio1" />{''}
-                                            Suhu >= 37.5 derajat Celcius
-                                        </Label>
-                                    </FormGroup>
+                                    <Row className="limitCol "> 
+                                        <Col sm="3">
+                                            <FormGroup className="d-inline pr-2">  
+                                                <Label className="rdoBtn" style={{left:"12px"}}>Anamnesis
+                                                <Input type="radio" name="radio1" /**value={1} onChange={handleAnswer1} checked={tbu_letargis === true} **/required/>
+                                                <span style={{left:"-15px"}} className="checkmark"></span>
+                                                </Label>
+                                            </FormGroup>
+                                        </Col>
+                                        <Col sm="3">
+                                            <FormGroup className="d-inline">
+                                                <Label className="rdoBtn" style={{left:"9px"}}>Teraba panas
+                                                <Input type="radio" name="radio1" /**value={2} onChange={handleAnswer1} checked={tbu_letargis === false}**/ /> 
+                                                <span style={{left:"-23px"}} className="checkmark"></span>
+                                                </Label>
+                                            </FormGroup>
+                                        </Col>
+                                        <Col sm="6">
+                                            <FormGroup className="d-inline">
+                                                <Label className="rdoBtn" style={{left:"12px"}}>Suhu >= 37.5 derajat Celcius
+                                                <Input type="radio" name="radio1" /**value={2} onChange={handleAnswer1} checked={tbu_letargis === false}**/ /> 
+                                                <span style={{left:"-20px"}} className="checkmark"></span>
+                                                </Label>
+                                            </FormGroup>
+                                        </Col>
+                                    </Row>
                                 </CardBody>
                             </Card>
                             <Card style={outlineColor} className="text-center w-75 mt-3" >
                                 <CardBody>
                                     <CardTitle className="h5"><b>Periksa! </b>Tentukan daerah endemis malaria</CardTitle>
-                                    <FormGroup check className="d-inline pr-2">
-                                        <Label cek>
-                                            <Input type="radio" name="radio2" />{''}
-                                            Tinggi
-                                        </Label>
-                                    </FormGroup>
-                                    <FormGroup check className="d-inline">
-                                        <Label cek>
-                                            <Input type="radio" name="radio2" />{''}
-                                            Rendah
-                                        </Label>
-                                    </FormGroup>
-                                    <FormGroup check className="d-inline pl-4">
-                                        <Label cek>
-                                            <Input type="radio" name="radio2" />{''}
-                                            Non Endemis
-                                        </Label>
-                                    </FormGroup>
+                                    <Row className="limitCol "> 
+                                        <Col sm="1">
+                                            
+                                        </Col>
+                                        <Col sm="3">
+                                            <FormGroup className="d-inline pr-2">  
+                                                <Label className="rdoBtn" style={{left:"10px"}}>Tinggi
+                                                <Input type="radio" name="radio2" /**value={1} onChange={handleAnswer1} checked={tbu_letargis === true} **/required/>
+                                                <span style={{left:"6px"}} className="checkmark"></span>
+                                                </Label>
+                                            </FormGroup>
+                                        </Col>
+
+                                        <Col sm="3">
+                                            <FormGroup className="d-inline">
+                                                <Label className="rdoBtn" style={{left:"10px"}}>Rendah
+                                                <Input type="radio" name="radio2" /**value={2} onChange={handleAnswer1} checked={tbu_letargis === false}**/ /> 
+                                                <span style={{left:"-1px"}} className="checkmark"></span>
+                                                </Label>
+                                            </FormGroup>
+                                        </Col>
+
+                                        <Col sm="4">
+                                            <FormGroup className="d-inline">
+                                                <Label className="rdoBtn" style={{left:"10px"}}>Non Endemis 
+                                                <Input type="radio" name="radio2" /**value={2} onChange={handleAnswer1} checked={tbu_letargis === false}**/ /> 
+                                                <span style={{left:"-1px"}} className="checkmark"></span>
+                                                </Label>
+                                            </FormGroup>
+                                        </Col>
+                                        <Col sm="1">
+                                            
+                                        </Col>
+                                    </Row>
                                 </CardBody>
                             </Card>
                         </Row>
                     </div>
                 </div>
-                <Row className="justify-content-between px-5 py-3">
+                <Row className="justify-content-between px-5 py-0">
                     <Col sm="4">
                         <Link to="Diare1" style={{textDecoration: "none"}}><Button color="danger" block><FontAwesomeIcon icon={faChevronLeft}/> Pemeriksaan Diare</Button></Link>
                     </Col>
                     <Col sm="4">
-                        <Link to="Demam2" style={{textDecoration: "none"}}><Button color="success" block >Selanjutnya <FontAwesomeIcon icon={faChevronRight}/></Button></Link>
+                        <Button type="submit" color="success" block >Selanjutnya <FontAwesomeIcon icon={faChevronRight}/></Button>
                     </Col>
                 </Row>
             </div>

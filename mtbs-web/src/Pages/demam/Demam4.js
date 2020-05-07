b887ec65ad1f9cf0834a3ecb2b68df28e6ee31b0
NaomiShinna <myrichardkun1@gmail.com>
2020-05-08 00:14:58 +0700

    [UPDATE]Memperbaiki radio button dan menambah comment

--- mtbs-web/src/Pages/demam/Demam4.js
+++ mtbs-web/src/Pages/demam/Demam4.js
@@ -4,6 +4,7 @@ import { FormGroup, Label, Input, Form, Card, CardBody, CardTitle, Button, Row, 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircle, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
 
+import '../../Assets/style/style.css';
 
 var outlineColor = {
     borderColor: '#41E8B3'
@@ -11,7 +12,7 @@ var outlineColor = {
 
 const Demam = (props) => {
     return (
-        <Form>
+        <Form /**onSubmit={handleSubmit}**/>
             <div className="w-100">
                 <div className="col-12">
                     <div className="d-flex justify-content-center mt-3">
@@ -58,7 +59,7 @@ const Demam = (props) => {
                         <p className="text-center"><b>Jika Daerah Non Endemis</b>, tanyakan riwayat bepergian ke daerah endemis
                         malaria dalam 2 minggu terakhir adn tentukan daerah endemis sesuai tempat yang dikunjungi</p>
                     </div>
-                    <div style={{minHeight: "500px"}}>
+                    <div style={{minHeight: "410px"}}>
                         <Row className="justify-content-around">
                             <Card style={outlineColor} className="text-center w-75" >
                                 <CardBody>
@@ -72,18 +73,30 @@ const Demam = (props) => {
                                     />
                                     <div>
                                         <h6>Ruam kemerahan di kulit yang meyeluruh</h6>
-                                        <FormGroup check className="d-inline pr-2">
-                                            <Label cek>
-                                                <Input type="radio" name="radio1" />{''}
-                                                Ya
-                                            </Label>
-                                        </FormGroup>
-                                        <FormGroup check className="d-inline">
-                                            <Label cek>
-                                                <Input type="radio" name="radio1" />{''}
-                                                Tidak
-                                            </Label>
-                                        </FormGroup>
+                                        <Row className="limitCol "> 
+                                            <Col  sm="3">
+                                            
+                                            </Col>
+                                            <Col sm="3">
+                                                <FormGroup className="d-inline pr-2">  
+                                                    <Label className="rdoBtn">Ya
+                                                    <Input type="radio" name="radio1" /**value={1} onChange={handleAnswer1} checked={tbu_letargis === true}**/ required/>
+                                                    <span style={{left:"20px"}} className="checkmark"></span>
+                                                    </Label>
+                                                </FormGroup>
+                                            </Col>
+                                            <Col sm="1">
+                                            
+                                            </Col>
+                                            <Col sm="3">
+                                                <FormGroup className="d-inline">
+                                                    <Label className="rdoBtn">Tidak
+                                                    <Input type="radio" name="radio1" /**value={2} onChange={handleAnswer1} checked={tbu_letargis === false}**/ /> 
+                                                    <span style={{left:"0px"}} className="checkmark"></span>
+                                                    </Label>
+                                                </FormGroup>
+                                            </Col>
+                                        </Row>
                                     </div>
                                     <hr
                                         style={{
@@ -94,18 +107,30 @@ const Demam = (props) => {
                                     />
                                     <div>
                                         <h6>Terdapat salah satu tanda berikut: batuk, pilek, mata merah</h6>
-                                        <FormGroup check className="d-inline pr-2">
-                                            <Label cek>
-                                                <Input type="radio" name="radio2" />{''}
-                                                Ya
-                                            </Label>
-                                        </FormGroup>
-                                        <FormGroup check className="d-inline">
-                                            <Label cek>
-                                                <Input type="radio" name="radio2" />{''}
-                                                Tidak
-                                            </Label>
-                                        </FormGroup>
+                                        <Row className="limitCol "> 
+                                            <Col  sm="3">
+                                            
+                                            </Col>
+                                            <Col sm="3">
+                                                <FormGroup className="d-inline pr-2">  
+                                                    <Label className="rdoBtn">Ya
+                                                    <Input type="radio" name="radio2" /**value={1} onChange={handleAnswer2} checked={tbu_letargis === true}**/ required/>
+                                                    <span style={{left:"20px"}} className="checkmark"></span>
+                                                    </Label>
+                                                </FormGroup>
+                                            </Col>
+                                            <Col sm="1">
+                                            
+                                            </Col>
+                                            <Col sm="3">
+                                                <FormGroup className="d-inline">
+                                                    <Label className="rdoBtn">Tidak
+                                                    <Input type="radio" name="radio2" /**value={2} onChange={handleAnswer2} checked={tbu_letargis === false}**/ /> 
+                                                    <span style={{left:"0px"}} className="checkmark"></span>
+                                                    </Label>
+                                                </FormGroup>
+                                            </Col>
+                                        </Row>
                                     </div>
 
                                 </CardBody>
@@ -113,12 +138,12 @@ const Demam = (props) => {
                         </Row>
                     </div>
                 </div>
-                <Row className="justify-content-between px-5 py-3">
+                <Row className="justify-content-between px-5 py-0">
                     <Col sm="4">
                         <Link to="Demam3" style={{textDecoration: "none"}}><Button color="danger" block><FontAwesomeIcon icon={faChevronLeft}/> Sebelumnya</Button></Link>
                     </Col>
                     <Col sm="4">
-                        <Link to="Demam5" style={{textDecoration: "none"}}><Button color="success" block >Selanjutnya <FontAwesomeIcon icon={faChevronRight}/></Button></Link>
+                        <Button type="submit" color="success" block >Selanjutnya <FontAwesomeIcon icon={faChevronRight}/></Button>
                     </Col>
                 </Row>
             </div>

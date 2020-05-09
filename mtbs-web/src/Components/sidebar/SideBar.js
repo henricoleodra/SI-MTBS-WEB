2721984ac4bdeb5ceb14f96e427b02b9d5edb99d
Henrico Leodra <leodra28@gmail.com>
2020-05-09 23:48:04 +0700

    [UPDATE] Side Bar Active

--- mtbs-web/src/Components/sidebar/SideBar.js
+++ mtbs-web/src/Components/sidebar/SideBar.js
@@ -56,7 +56,7 @@ const SideBar = (props) => {
         {
             'title' : 'Demam',
             'link' : 'Demam1',
-            'active' : url==='DemamYaTidak' || url==='DemamDaerah' || url==='DemamKonfirmasiDaerah' || url==='Demam',
+            'active' : url==='DemamYaTidak' || url==='DemamDaerah' || url==='DemamKonfirmasiDaerah' || url==='DemamPerhatian' || url==='Demam',
             'color' : 'dark',
             'disabled' : compStatus.demam
         },

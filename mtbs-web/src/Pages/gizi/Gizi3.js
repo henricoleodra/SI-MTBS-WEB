08bb7b0b64f9056a0c69a5e8c4a9f9a90d9322bb
friska christiana <christianafriska@gmail.com>
2020-05-09 22:21:25 +0700

    [UPDATE] hlaman gizi3

--- mtbs-web/src/Pages/gizi/Gizi3.js
+++ mtbs-web/src/Pages/gizi/Gizi3.js
@@ -28,14 +28,14 @@ const Gizi3 = (props) => {
                     <div className="mt-2">
                         <h3 className="text-center font-weight-bold">Status Gizi</h3>
                         <hr
-                        style={{
-                            color: "#41E8B3",
-                            backgroundColor: "#41E8B3",
-                            height: 5
-                        }}
-                    />
+                            style={{
+                                color: "#41E8B3",
+                                backgroundColor: "#41E8B3",
+                                height: 5
+                            }}
+                        />
                     </div>
-                    <div style={{minHeight: "475px"}}>
+                    <div style={{ minHeight: "475px" }}>
                         <Row className="justify-content-center">
                             <Card style={outlineColor} className="text-center w-75 mt-3" >
                                 <CardBody>
@@ -50,34 +50,58 @@ const Gizi3 = (props) => {
                                     <div className="d-flex justify-content-around pt-2">
                                         <div className="d-line">
                                             <h6>Apakah anda tanda bahaya umum?</h6>
-                                            <FormGroup check className="d-inline pr-2">
+                                            {/* <FormGroup check className="d-inline pr-2">
                                                 <Label className="rdoBtn">Ya
-                                                <Input type="radio" name="radio5" /**value={1} onChange={handleAnswer1} checked={tbu_letargis === true}**/ required/>
+                                                <Input type="radio" name="radio5" value={1} onChange={handleAnswer1} checked={tbu_letargis === true} required/>
                                                 <span style={{left:"20px"}} className="checkmark"></span>
                                                 </Label>
                                             </FormGroup>
                                             <FormGroup check className="d-inline">
                                                 <Label className="rdoBtn">Tidak
-                                                <Input type="radio" name="radio5" /**value={1} onChange={handleAnswer1} checked={tbu_letargis === true}**/ required/>
+                                                <Input type="radio" name="radio5" value={1} onChange={handleAnswer1} checked={tbu_letargis === true} required/>
                                                 <span style={{left:"20px"}} className="checkmark"></span>
                                                 </Label>
-                                            </FormGroup>
-                                                
+                                            </FormGroup> */}
+
+                                            <Row className="limitCol">
+                                                <Col sm="6">
+                                                    <FormGroup className="d-inline pr-2">
+                                                        <Label className="rdoBtn" style={{ left: "20px" }}>Ya
+                                                            <Input type="radio" name="radio1" /**value={1} onChange={handleTelinga} checked={telinga === true}**/ required />
+                                                            <span style={{ left: "20px" }} className="checkmark"></span>
+                                                        </Label>
+                                                    </FormGroup>
+                                                </Col>
+                                                <Col sm="6">
+                                                    <FormGroup className="d-inline">
+                                                        <Label className="rdoBtn">Tidak
+                                                            <Input type="radio" name="radio1" /**value={2} onChange={handleTelinga} checked={telinga === false}**/ />
+                                                            <span style={{ left: "0px" }} className="checkmark"></span>
+                                                        </Label>
+                                                    </FormGroup>
+                                                </Col>
+                                            </Row>
                                         </div>
                                         <div className="d-line">
                                             <h6>Apakah ada klasifikasi berat?</h6>
-                                            <FormGroup check className="d-inline pr-2">
-                                                <Label className="rdoBtn">Ya
-                                                <Input type="radio" name="radio5" /**value={1} onChange={handleAnswer1} checked={tbu_letargis === true}**/ required/>
-                                                <span style={{left:"20px"}} className="checkmark"></span>
-                                                </Label>
-                                            </FormGroup>
-                                            <FormGroup check className="d-inline">
-                                                <Label className="rdoBtn">Tidak
-                                                <Input type="radio" name="radio5" /**value={1} onChange={handleAnswer1} checked={tbu_letargis === true}**/ required/>
-                                                <span style={{left:"20px"}} className="checkmark"></span>
-                                                </Label>
-                                            </FormGroup>
+                                            <Row className="limitCol">
+                                                <Col sm="6">
+                                                    <FormGroup className="d-inline pr-2">
+                                                        <Label className="rdoBtn"style={{ left: "20px" }}>Ya
+                                                            <Input type="radio" name="radio2" /**value={1} onChange={handleTelinga} checked={telinga === true}**/ required />
+                                                            <span style={{ left: "5px" }} className="checkmark"></span>
+                                                        </Label>
+                                                    </FormGroup>
+                                                </Col>
+                                                <Col sm="6">
+                                                    <FormGroup className="d-inline">
+                                                        <Label className="rdoBtn" style={{ left: "5px"}}>Tidak
+                                                            <Input type="radio" name="radio2" /**value={2} onChange={handleTelinga} checked={telinga === false}**/ />
+                                                            <span style={{ left: "0px" }} className="checkmark"></span>
+                                                        </Label>
+                                                    </FormGroup>
+                                                </Col>
+                                            </Row>
                                         </div>
                                     </div>
                                     <hr
@@ -88,31 +112,31 @@ const Gizi3 = (props) => {
                                         }}
                                     />
                                     <div className="d-line">
-                                            <h5>Jika tidak ada komplikasi media, nilai pemberian ASI pad aanak umur lebih kecil dari 6 bulan</h5>
-                                            <h5>Apakah anaka memiliki masalah pemberian ASI?</h5>
-                                            <Row className="limitCol">
-                                                <Col sm="3">
-                                                </Col>
-                                                <Col sm="3">
-                                                    <FormGroup className="d-inline pr-2">  
-                                                        <Label className="rdoBtn">Ya
-                                                        <Input type="radio" name="radio6" /**value={1} onChange={handleAnswer1} checked={tbu_letargis === true}**/ required/>
-                                                        <span style={{left:"20px"}} className="checkmark"></span>
-                                                        </Label>
-                                                    </FormGroup>
-                                                </Col>
-                                                <Col sm="1">
-                                                </Col>
-                                                <Col sm="3">
-                                                    <FormGroup className="d-inline">
-                                                        <Label className="rdoBtn">Tidak
-                                                        <Input type="radio" name="radio6" /**value={2} onChange={handleAnswer1} checked={tbu_letargis === false}**/ /> 
-                                                        <span style={{left:"0px"}} className="checkmark"></span>
-                                                        </Label>
-                                                    </FormGroup>
-                                                </Col>
-                                            </Row>
-                                        </div>
+                                        <h5>Jika tidak ada komplikasi media, nilai pemberian ASI pad aanak umur lebih kecil dari 6 bulan</h5>
+                                        <h5>Apakah anaka memiliki masalah pemberian ASI?</h5>
+                                        <Row className="limitCol">
+                                            <Col sm="3">
+                                            </Col>
+                                            <Col sm="3">
+                                                <FormGroup className="d-inline pr-2">
+                                                    <Label className="rdoBtn">Ya
+                                                        <Input type="radio" name="radio6" /**value={1} onChange={handleAnswer1} checked={tbu_letargis === true}**/ required />
+                                                        <span style={{ left: "20px" }} className="checkmark"></span>
+                                                    </Label>
+                                                </FormGroup>
+                                            </Col>
+                                            <Col sm="1">
+                                            </Col>
+                                            <Col sm="3">
+                                                <FormGroup className="d-inline">
+                                                    <Label className="rdoBtn">Tidak
+                                                        <Input type="radio" name="radio6" /**value={2} onChange={handleAnswer1} checked={tbu_letargis === false}**/ />
+                                                        <span style={{ left: "0px" }} className="checkmark"></span>
+                                                    </Label>
+                                                </FormGroup>
+                                            </Col>
+                                        </Row>
+                                    </div>
                                 </CardBody>
                             </Card>
                         </Row>
@@ -120,10 +144,10 @@ const Gizi3 = (props) => {
                 </div>
                 <Row className="justify-content-between px-5 py-3">
                     <Col sm="4">
-                        <Link to="Gizi2" style={{textDecoration: "none"}}><Button color="danger" block><FontAwesomeIcon icon={faChevronLeft}/> Sebelumnya</Button></Link>
+                        <Link to="Gizi2" style={{ textDecoration: "none" }}><Button color="danger" block><FontAwesomeIcon icon={faChevronLeft} /> Sebelumnya</Button></Link>
                     </Col>
                     <Col sm="4">
-                        <Button type="submit" color="success" block >Pemeriksaan Anemia <FontAwesomeIcon icon={faChevronRight}/></Button>
+                        <Button type="submit" color="success" block >Pemeriksaan Anemia <FontAwesomeIcon icon={faChevronRight} /></Button>
                     </Col>
                 </Row>
             </div>

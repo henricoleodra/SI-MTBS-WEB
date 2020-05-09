8897ae4b3d0d43c9f1c5236f3803dd598cab0943
NaomiShinna <myrichardkun1@gmail.com>
2020-05-10 00:44:09 +0700

    [UPDATE]HandleSubmit 9 dan memperbaiki navigasi menjadi 9 button(radio)

--- mtbs-web/src/Pages/demam/Demam9.js
+++ mtbs-web/src/Pages/demam/Demam9.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FormGroup, Label, Input, Form, Card, CardBody, CardTitle, Button, Row, Col } from "reactstrap";
+import { useDispatch, useSelector } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircle, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import axios from 'axios';
+
+import { KlasifikasiDemamChange, AnsDemamChange } from '../../Actions';
+import { compStatusChange } from '../../Actions';
 
 import '../../Assets/style/style.css';
 
@@ -11,14 +16,43 @@ var outlineColor = {
 }
 
 const Demam = (props) => {
+
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const ansDemam = useSelector(state => state.ansDemam);
+    let [demam_hasilUjiTorniket, set_demam_hasilUjiTorniket] = useState(ansDemam.demam_hasilUjiTorniket);
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        axios.post(`/Demam`, {
+            ansDemam: ansDemam
+        })
+            .then(res => {
+                dispatchEvent(KlasifikasiDemamChange('DEMAM_KLASIFIKASI', res.data.hasilKlasifikasi));
+                dispatchEvent(KlasifikasiDemamChange('DEMAM_STATUS', res.data.statusKlasifikasi));
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        history.push("TelingaYaTidak");
+        dispatch(compStatusChange('DEMAM'));
+    }
+
+    const handleAnswer1 = event => {
+        if (event.target.value == 1) {
+            set_demam_hasilUjiTorniket(true);
+            dispatch(AnsDemamChange('UJI_TORNIKET', true));
+        } else {
+            set_demam_hasilUjiTorniket(false);
+            dispatch(AnsDemamChange('UJI_TORNIKET', false));
+        }
+    }
+
     return (
-        <Form /**onSubmit={handleSubmit}**/>
+        <Form onSubmit={handleSubmit}>
             <div className="w-100">
                 <div className="col-12">
                     <div className="d-flex justify-content-center mt-3">
-                        <div className="p-2">
-                            <FontAwesomeIcon icon={faCircle} className="text-muted" />
-                        </div>
                         <div className="p-2">
                             <FontAwesomeIcon icon={faCircle} className="text-muted" />
                         </div>
@@ -58,7 +92,7 @@ const Demam = (props) => {
                         />
                         {/* <p className="text-center"><b>Jika demam 2 hari sampai dengan 7 hari, tanya dan periksa</b></p> */}
                     </div>
-                    <div style={{minHeight: "475px"}}>
+                    <div style={{ minHeight: "475px" }}>
                         <Row className="justify-content-around">
                             <Card style={outlineColor} className="text-center w-75 mt-3" >
                                 <CardBody>
@@ -71,26 +105,26 @@ const Demam = (props) => {
                                         }}
                                     />
                                     <h5>Hasil Uji Torniket</h5>
-                                    <Row className="limitCol "> 
-                                        <Col  sm="3">
-                                        
+                                    <Row className="limitCol ">
+                                        <Col sm="3">
+
                                         </Col>
                                         <Col sm="3">
-                                            <FormGroup className="d-inline pr-2">  
+                                            <FormGroup className="d-inline pr-2">
                                                 <Label className="rdoBtn">Positif
-                                                <Input type="radio" name="radio1" /**value={1} onChange={handleAnswer1} checked={tbu_letargis === true}**/ required/>
-                                                <span style={{left:"6px"}} className="checkmark"></span>
+                                                <Input type="radio" name="radio1" value={1} onChange={handleAnswer1} checked={demam_hasilUjiTorniket === true} required />
+                                                    <span style={{ left: "6px" }} className="checkmark"></span>
                                                 </Label>
                                             </FormGroup>
                                         </Col>
                                         <Col sm="1">
-                                        
+
                                         </Col>
                                         <Col sm="3">
                                             <FormGroup className="d-inline">
                                                 <Label className="rdoBtn">Negatif
-                                                <Input type="radio" name="radio1" /**value={2} onChange={handleAnswer1} checked={tbu_letargis === false}**/ /> 
-                                                <span style={{left:"0px"}} className="checkmark"></span>
+                                                <Input type="radio" name="radio1" value={2} onChange={handleAnswer1} checked={demam_hasilUjiTorniket === false} />
+                                                    <span style={{ left: "0px" }} className="checkmark"></span>
                                                 </Label>
                                             </FormGroup>
                                         </Col>
@@ -108,10 +142,10 @@ const Demam = (props) => {
                 </div>
                 <Row className="justify-content-between px-5 py-0">
                     <Col sm="4">
-                        <Link to="Demam9" style={{textDecoration: "none"}}><Button color="danger" block><FontAwesomeIcon icon={faChevronLeft}/> Sebelumnya</Button></Link>
+                        <Link to="Demam9" style={{ textDecoration: "none" }}><Button color="danger" block><FontAwesomeIcon icon={faChevronLeft} /> Sebelumnya</Button></Link>
                     </Col>
                     <Col sm="4">
-                        <Button type="submit" color="success" block >Selanjutnya <FontAwesomeIcon icon={faChevronRight}/></Button>
+                        <Button type="submit" color="success" block >Selanjutnya <FontAwesomeIcon icon={faChevronRight} /></Button>
                     </Col>
                 </Row>
             </div>

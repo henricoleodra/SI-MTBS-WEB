47008c98df153b622ae462706f32df6b59dd1c8f
friska christiana <christianafriska@gmail.com>
2020-05-10 00:17:39 +0700

    [UDPATE] Demam6 handlesubmit

--- mtbs-web/src/Pages/demam/Demam6.js
+++ mtbs-web/src/Pages/demam/Demam6.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link,useHistory } from 'react-router-dom';
 import { FormGroup, Label, Input, Form, Card, CardBody, CardTitle, Button, Row, Col } from "reactstrap";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircle, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
 
+// Actions
+import { KlasifikasiDemamChange, AnsDemamChange } from '../../Actions';
 import '../../Assets/style/style.css';
 
 var outlineColor = {
@@ -11,6 +15,71 @@ var outlineColor = {
 }
 
 const Demam = (props) => {
+    // const history = useHistory();
+    // const dispatch = useDispatch();
+    // const ansDiare = useSelector(state => state.ansDemam);
+    // const klasifikasiDiare = useSelector(state => state.klasifikasiDemam);
+    // let[demam_isLukaMulut, set_demam_isLukaMulut] = useState(ansDemam.demam_isLukaMulut);
+    // let[demam_descLukaMulut, set_demam_descLukaMulut] = useState(ansDemam.demam_descLukaMulut);
+    // let[demam_nanahDiMata, set_demam_nanahDiMata] = useState(ansDemam.demam_nanahDiMata);
+    // let[demam_korneaKeruh, set_demam_korneaKeruh] = useState(ansDemam.demam_korneaKeruh);
+
+    // const handleSubmit = event => {
+    //     event.preventDefault();
+    //     axios.post(`/Demam`, {
+    //         ansDemam : ansDemam
+    //     })
+    //     .then(res => {
+    //         dispatch(KlasifikasiDemamChange('DEMAM_KLASIFIKASI', res.data.hasilKlasifkasi));
+    //         dispatch(KlasifikasiDemamChange('DEMAM_STATUS', res.data.statusKlasifikasi));
+    //     })
+    //     .catch(err=>{
+    //     console.log(err);
+    //     });
+    //     dispatch(KlasifikasiDemamChange('Demam_6', true));
+    //     history.push("Demam7"); 
+    //     dispatch(compStatusChange('TELINGA'));
+    // }
+    // const handleAnswer1 = event =>{
+    //     if(event.target.value == 1){
+    //         set_demam_isLukaMulut(true);
+    //         dispatch(AnsDemamChange('LUKA_MULUT', true));
+    //     }else{
+    //         set_demam_isLukaMulut(false);
+    //         dispatch(AnsDemamChange('LETARGIS', false));
+    //     }
+    // }
+
+    // const handleAnswer2 = event =>{
+    //     if(event.target.value == 1){
+    //         set_demam_isLukaMulut(true);
+    //         dispatch(AnsDemamChange('LUKA_MULUT', true));
+    //     }else{
+    //         set_demam_isLukaMulut(false);
+    //         dispatch(AnsDemamChange('LETARGIS', false));
+    //     }
+    // }
+
+    // const handleAnswer3 = event =>{
+    //     if(event.target.value == 1){
+    //         set_demam_isLukaMulut(true);
+    //         dispatch(AnsDemamChange('LUKA_MULUT', true));
+    //     }else{
+    //         set_demam_isLukaMulut(false);
+    //         dispatch(AnsDemamChange('LETARGIS', false));
+    //     }
+    // }
+
+    // const handleAnswer4 = event =>{
+    //     if(event.target.value == 1){
+    //         set_demam_isLukaMulut(true);
+    //         dispatch(AnsDemamChange('LUKA_MULUT', true));
+    //     }else{
+    //         set_demam_isLukaMulut(false);
+    //         dispatch(AnsDemamChange('LETARGIS', false));
+    //     }
+    // }
+
     return (
         <Form /**onSubmit={handleSubmit}**/>
             <div className="w-100">
@@ -70,7 +139,7 @@ const Demam = (props) => {
                                         <Col sm="3">
                                             <FormGroup className="d-inline pr-2">  
                                                 <Label className="rdoBtn">Ya
-                                                <Input type="radio" name="radio1" /**value={1} onChange={handleAnswer1} checked={tbu_letargis === true}**/ required/>
+                                                <Input type="radio" name="radio1" /**value={1} onChange={handleAnswer1} checked={demam_isLukaMulut === true}**/ required/>
                                                 <span style={{left:"20px"}} className="checkmark"></span>
                                                 </Label>
                                             </FormGroup>
@@ -81,7 +150,7 @@ const Demam = (props) => {
                                         <Col sm="3">
                                             <FormGroup className="d-inline">
                                                 <Label className="rdoBtn">Tidak
-                                                <Input type="radio" name="radio1" /**value={2} onChange={handleAnswer1} checked={tbu_letargis === false}**/ /> 
+                                                <Input type="radio" name="radio1" /**value={2} onChange={handleAnswer1} checked={demam_isLukaMulut === false}**/ /> 
                                                 <span style={{left:"5px"}} className="checkmark"></span>
                                                 </Label>
                                             </FormGroup>
@@ -96,7 +165,7 @@ const Demam = (props) => {
                                             <Col sm="3">
                                                 <FormGroup className="d-inline pr-2">  
                                                     <Label style={{left:"14px"}} className="chckBtn">Dalam
-                                                    <Input type="checkbox" name="checkBox1" /**value={1} onChange={handleAnswer1} checked={tbu_letargis === true}**/ required/>
+                                                    <Input type="checkbox" name="checkBox1" /**value={1} onChange={handleAnswer2} checked={demam_descLukaMulut === true}**/ required/>
                                                     <span style={{left:"3px"}} className="checkmarkBox"></span>
                                                     </Label>
                                                 </FormGroup>
@@ -107,7 +176,7 @@ const Demam = (props) => {
                                             <Col sm="3">
                                                 <FormGroup className="d-inline">
                                                     <Label style={{left:"-7px"}} className="chckBtn">Luas
-                                                    <Input type="checkbox" name="checkBox1" /**value={2} onChange={handleAnswer1} checked={tbu_letargis === false}**/ /> 
+                                                    <Input type="checkbox" name="checkBox1" /**value={2} onChange={handleAnswer2} checked={demam_descLukaMulut === false}**/ /> 
                                                     <span style={{left:"12px"}} className="checkmarkBox"></span>
                                                     </Label>
                                                 </FormGroup>
@@ -126,7 +195,7 @@ const Demam = (props) => {
                                         <Col sm="3">
                                             <FormGroup className="d-inline pr-2">  
                                                 <Label className="rdoBtn">Ya
-                                                <Input type="radio" name="radio3" /**value={1} onChange={handleAnswer1} checked={tbu_letargis === true}**/ required/>
+                                                <Input type="radio" name="radio3" /**value={1} onChange={handleAnswer3} checked={demam_nanahDiMata === true}**/ required/>
                                                 <span style={{left:"20px"}} className="checkmark"></span>
                                                 </Label>
                                             </FormGroup>
@@ -137,7 +206,7 @@ const Demam = (props) => {
                                         <Col sm="3">
                                             <FormGroup className="d-inline">
                                                 <Label className="rdoBtn">Tidak
-                                                <Input type="radio" name="radio3" /**value={2} onChange={handleAnswer1} checked={tbu_letargis === false}**/ /> 
+                                                <Input type="radio" name="radio3" /**value={2} onChange={handleAnswer3} checked={demam_nanahDiMata === false}**/ /> 
                                                 <span style={{left:"5px"}} className="checkmark"></span>
                                                 </Label>
                                             </FormGroup>
@@ -155,7 +224,7 @@ const Demam = (props) => {
                                         <Col sm="3">
                                             <FormGroup className="d-inline pr-2">  
                                                 <Label className="rdoBtn">Ya
-                                                <Input type="radio" name="radio4" /**value={1} onChange={handleAnswer1} checked={tbu_letargis === true}**/ required/>
+                                                <Input type="radio" name="radio4" /**value={1} onChange={handleAnswer4} checked={demam_korneaKeruh === true}**/ required/>
                                                 <span style={{left:"20px"}} className="checkmark"></span>
                                                 </Label>
                                             </FormGroup>
@@ -166,7 +235,7 @@ const Demam = (props) => {
                                         <Col sm="3">
                                             <FormGroup className="d-inline">
                                                 <Label className="rdoBtn">Tidak
-                                                <Input type="radio" name="radio4" /**value={2} onChange={handleAnswer1} checked={tbu_letargis === false}**/ /> 
+                                                <Input type="radio" name="radio4" /**value={2} onChange={handleAnswer4} checked={demam_korneaKeruh === false}**/ /> 
                                                 <span style={{left:"5px"}} className="checkmark"></span>
                                                 </Label>
                                             </FormGroup>

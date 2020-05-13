f5a074952ba43334d93ec22525dd73ec22404754
Henrico Leodra <leodra28@gmail.com>
2020-05-13 20:04:08 +0700

    [UPDATE] Beranda

--- mtbs-web/src/Pages/beranda/Beranda.js
+++ mtbs-web/src/Pages/beranda/Beranda.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Row, Col, Card, CardBody, CardTitle, CardText, CardImg, Button} from 'reactstrap'
+import { Row, Col, Card, CardBody, CardTitle, CardText, CardImg, Button} from 'reactstrap';
 
 // Import Components
-import NavBar from './../../Components/navbar/NavBar'
+import NavBar from './../../Components/navbar/NavBar';
 
 //Import Image
-import kidBoy from './../../Assets/image/kidboy.png'
+// import pemeriksaan from './../../Assets/image/medicine.png';
+// import buku from './../../Assets/image/book.png';
+// import baby from './../../Assets/image/baby.png';
+import pemeriksaan from './../../Assets/image/pemeriksaan.png';
+import mtbs from './../../Assets/image/mtbs.png';
+import dataanak from './../../Assets/image/data-anak.png';
 
 const Beranda = (props) =>{
     return(
         <div>
             <NavBar />
-            <Row className="mt-3 justify-content-around">
+            <Row className="mt-3 justify-content-around" style={{width: '100%'}}>
                 <Col md="3" className="">
                     <Card style={{minHeight: '530px', backgroundColor : '#41E8B3', borderColor : '#41E8B3'}} className="shadow-lg">
                         <CardTitle className="text-center text-white"><h1>Klasifikasi</h1></CardTitle>
                         <CardBody className="justify-content-center">
-                            <CardImg src={kidBoy} width='25%' height='50%'/>
-                            <CardText className="text-center">
+                            <CardImg src={pemeriksaan} width='25%' height='200px'/>
+                            <CardText className="text-center mt-2">
                                 Cintai buah hati dengan melakukan test kesehatan, apakah buat hati sehat, 
                                 dan terimuninasi sesuai dengan waktunya.
                             </CardText>
@@ -32,8 +37,8 @@ const Beranda = (props) =>{
                     <Card style={{minHeight: '530px', backgroundColor : '#41E8B3', borderColor : '#41E8B3'}} className="shadow-lg">
                         <CardTitle className="text-center text-white"><h1>Apa Itu MTBS?</h1></CardTitle>
                         <CardBody className="justify-content-center">
-                            <CardImg src={kidBoy} width='25%' height='50%'/>
-                            <CardText className="text-center">
+                            <CardImg src={mtbs} width='25%' height='200px'/>
+                            <CardText className="text-center mt-2">
                                 MTBS adalah Manajemen Terpadu Balita Sakit yang berfungsi untuk mengecek kesehatan dari balita
                             </CardText>
                         </CardBody>
@@ -46,8 +51,8 @@ const Beranda = (props) =>{
                     <Card style={{minHeight: '530px', backgroundColor : '#41E8B3', borderColor : '#41E8B3'}} className="shadow-lg">
                         <CardTitle className="text-center text-white"><h1>Riwayat Anak</h1></CardTitle>
                         <CardBody className="justify-content-center">
-                            <CardImg src={kidBoy} width='25%' height='50%'/>
-                            <CardText className="text-center">
+                            <CardImg src={dataanak} width='25%' height='200px'/>
+                            <CardText className="text-center mt-2">
                                 Mencari riwayat data anak yang sudah pernah melakukan pendaftaran dan pemeriksaan
                             </CardText>
                         </CardBody>

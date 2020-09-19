7cb51c486ea324af4b9636f3beb471a5c33f366f
Henrico Leodra <leodra28@gmail.com>
2020-09-19 14:19:36 +0700

    [ADD] Redux Persist

--- mtbs-web/src/index.js
+++ mtbs-web/src/index.js
@@ -1,49 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 import './Config/index.scss'
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import rootReducer from './Reducers';
-
-const saveToLocalStorage = (state) => {
-    try{
-        const serializedState = JSON.stringify(state);
-        localStorage.setItem('state', serializedState);
-    } catch(e) {
-        console.log(e);
-    }
-}
-
-const loadFromLocalStorage = () => {
-    try{
-        const serializedState = localStorage.getItem('state');
-        if(serializedState === null) return undefined
-        return JSON.parse(serializedState)
-    } catch(e) {
-        console.log(e);
-        return undefined;
-    }
-}
-
-// const persistedState = loadFromLocalStorage();
-
-const store = createStore(
-    rootReducer
-    // ,persistedState
-    // , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+import { persistor, store } from './Config/store';
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <PersistGate persistor={persistor}>
+            <App />
+        </PersistGate>
     </Provider>
     , document.getElementById('root')
 );
-
-store.subscribe(() => saveToLocalStorage(store.getState()));
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();

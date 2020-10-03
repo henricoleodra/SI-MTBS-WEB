d3fcf3efad170a623931dca4c4589dc121b23579
friskac <christianafriska@gmail.com>
2020-10-03 21:56:44 +0700

    [UPDATE]button mulaiPemeriksaan

--- mtbs-web/src/Containers/mulaipemeriksaan/MulaiPemeriksaan.js
+++ mtbs-web/src/Containers/mulaipemeriksaan/MulaiPemeriksaan.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom'
-import { Row, Col, Card, CardTitle, CardText } from 'reactstrap';
+import { Row, Col, Card, CardTitle, CardText, Container } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { useDispatch } from 'react-redux';
@@ -13,16 +13,40 @@ const MulaiPemeriksaan = (props) =>{
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(reset('RESET_STORE'));
-     }, []);
+    }, []);
 
     return(
         <div>
             <HeaderTitle title="Mulai Pemeriksaan"/>
-            <div className="mt-2">
+            <div className="mt-3">
                 <h3 className="text-center">Apakah Anak sudah pernah melakukan pemeriksaan ?</h3>
             </div>
-            <Row className="justify-content-end mt-3 ml-0 mr-0" style={{width : "100%"}}>
-                <Col xs="6" >
+            <Row className=" mt-3 ml-0 mr-0">
+                <Col xs="6">
+                    <Container className="p-5" style={{width:"75%"}}>
+                        <Link to="PencarianDataAnak" style={{textDecoration: 'none'}}>
+                            <Card className="shadow rounded" style={{backgroundColor: '#46d0fe', border: '0'}}>
+                                <CardText className="d-flex justify-content-center mt-2">
+                                    <h1><FontAwesomeIcon icon={faCheck} className="text-white fa-7x"/></h1>
+                                </CardText>
+                                <CardTitle className="text-white text-center"><h1>YA</h1></CardTitle>
+                            </Card>
+                        </Link>
+                    </Container>
+                </Col>
+                <Col xs="6">
+                    <Container className="p-5" style={{width:"75%"}}>
+                        <Link to="IsiDataAnak/1" style={{textDecoration: 'none'}}>
+                            <Card className="shadow rounded" style={{backgroundColor: '#fe8d3b', border: '0'}}>
+                                <CardText className="d-flex justify-content-center mt-2">
+                                    <h1><FontAwesomeIcon icon={faTimes} className="text-white fa-7x"/></h1>
+                                </CardText>
+                                <CardTitle className="text-white text-center"><h1>TIDAK</h1></CardTitle>
+                            </Card>
+                        </Link>
+                    </Container>
+                </Col>
+                {/* <Col xs="6" >
                     <Link to="PencarianDataAnak" style={{textDecoration: 'none'}}>
                         <Card style={{minHeight: '400px'}} className="bg-success shadow rounded">
                             <CardText className="d-flex justify-content-center mt-2">
@@ -41,7 +65,7 @@ const MulaiPemeriksaan = (props) =>{
                             <CardTitle className="text-white text-center"><h1>TIDAK</h1></CardTitle>
                         </Card>
                     </Link>
-                </Col>
+                </Col> */}
             </Row>
             
             

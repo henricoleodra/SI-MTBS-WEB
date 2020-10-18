54c1a41c4ec8b62073c530c7849afe38daad9616
Henrico Leodra <leodra28@gmail.com>
2020-10-19 00:03:23 +0700

    [UPDATE] Else If Tindakan

--- api-mtbs-web/models/tindakan.js
+++ api-mtbs-web/models/tindakan.js
@@ -2,7 +2,8 @@ const processTindakan = (data) => {
   let res = Array();
   let tbu = Array();
   let bsb = Array();
-
+  let diare = Array();
+  let demam = Array();
   let telinga = Array();
   let gizi = Array();
   let anemia = Array();
@@ -21,15 +22,14 @@ const processTindakan = (data) => {
   }
 
   //batuk
-  if (data.batuk.klasifikasiBatuk.bsb_klasifikasi === "Pneumonia berat"){
+  if (data.batuk.klasifikasiBatuk.bsb_klasifikasi === "Pneumonia berat") {
     bsb.push(
       "Beri Oksigen maksimal 2-3 liter/menit dengan menggunakan nasal prong",
       "Beri dosis pertama antibiotik yang sesuai",
       "RUJUK SEGERA"
     );
     res.push(bsb);
-  }
-  if(data.batuk.klasifikasiBatuk.bsb_klasifikasi === "Pneumonia"){
+  } else if (data.batuk.klasifikasiBatuk.bsb_klasifikasi === "Pneumonia") {
     bsb.push(
       "Beri amoksisilin 2x sehari sela ma 3 hr atau 5 hr",
       "Beri pelega tenggorokan dan pereda batuk yang aman",
@@ -39,8 +39,9 @@ const processTindakan = (data) => {
       "Kunjungan ulang 2 hari"
     );
     res.push(bsb);
-  }
-  if(data.batuk.klasifikasiBatuk.bsb_klasifikasi === "Batuk bukan pneumonia"){
+  } else if (
+    data.batuk.klasifikasiBatuk.bsb_klasifikasi === "Batuk bukan pneumonia"
+  ) {
     bsb.push(
       "Beri pelega tenggorokan dan pereda batuk yang aman",
       "Obati wheezing bila ada",
@@ -56,15 +57,17 @@ const processTindakan = (data) => {
   //Demam
 
   //Telinga
-  if(data.telinga.klasifikasiTelinga.telinga_klasifikasi === "Mastoiditis"){
+  if (data.telinga.klasifikasiTelinga.telinga_klasifikasi === "Mastoiditis") {
     telinga.push(
       "Beri dosis pertama antibiotik yang sesuai",
       "Beri dosis pertama parasetamol untuk mengatasi nyeri",
-      "RUJUK SEGERA",
+      "RUJUK SEGERA"
     );
     res.push(telinga);
-  }
-  if(data.telinga.klasifikasiTelinga.telinga_klasifikasi === "Infeksi Telinga Kronis"){
+  } else if (
+    data.telinga.klasifikasiTelinga.telinga_klasifikasi ===
+    "Infeksi Telinga Kronis"
+  ) {
     telinga.push(
       "Beri antibiotik yang sesuai selama 7 hari",
       "Beri parasetamol untuk mengatasi nyeri",
@@ -72,24 +75,29 @@ const processTindakan = (data) => {
       "Kunjungan ulang 5 hari"
     );
     res.push(telinga);
-  }
-  if(data.telinga.klasifikasiTelinga.telinga_klasifikasi === "Infeksi Telinga Akut"){
+  } else if (
+    data.telinga.klasifikasiTelinga.telinga_klasifikasi ===
+    "Infeksi Telinga Akut"
+  ) {
     telinga.push(
       "Keringkan telinga dengan bahan penyerap setelah dicuci dengan NaCl 0,9% atau H2O23%",
       "Beri tetes telinga yang sesuai",
-      "Kunjungan ulang 5 hari",
+      "Kunjungan ulang 5 hari"
     );
     res.push(telinga);
-  }
-  if(data.telinga.klasifikasiTelinga.telinga_klasifikasi === "Tidak Ada Infeksi Telinga"){
-    telinga.push(
-      "Tangani masalah telinga yang ditemukan"
-    );
+  } else if (
+    data.telinga.klasifikasiTelinga.telinga_klasifikasi ===
+    "Tidak Ada Infeksi Telinga"
+  ) {
+    telinga.push("Tangani masalah telinga yang ditemukan");
     res.push(telinga);
   }
 
   //Gizi
-  if(data.gizi.klasifikasiGizi.gizi_klasifikasi === "Sangat Kurus Dengan Komplikasi"){
+  if (
+    data.gizi.klasifikasiGizi.gizi_klasifikasi ===
+    "Sangat Kurus Dengan Komplikasi"
+  ) {
     gizi.push(
       "Beri dosis pertama antibiotik yang sesuai",
       "Beri Vitamin A dosis pertama",
@@ -98,8 +106,10 @@ const processTindakan = (data) => {
       "RUJUK SEGERA"
     );
     res.push(gizi);
-  }
-  if(data.gizi.klasifikasiGizi.gizi_klasifikasi === "Sangat Kurus Tanpa Komplikasi"){
+  } else if (
+    data.gizi.klasifikasiGizi.gizi_klasifikasi ===
+    "Sangat Kurus Tanpa Komplikasi"
+  ) {
     gizi.push(
       "Beri antibiotik yang sesuai selama 5 hari",
       "Beri Vitamin A dosis pertama",
@@ -110,16 +120,14 @@ const processTindakan = (data) => {
       "Kunjungan ulang 7 hari"
     );
     res.push(gizi);
-  }
-  if(data.gizi.klasifikasiGizi.gizi_klasifikasi === "Kurus"){
+  } else if (data.gizi.klasifikasiGizi.gizi_klasifikasi === "Kurus") {
     gizi.push(
       "Lakukan Penilaian Pemberian Makan pada anak dan nasihati sesuai Anjuran Makan Untuk Anak Sehat Maupun Sakit. Bila ada masalah pemberian makan, kunjungan ulang 7 hari",
       "RUJUK untuk penilaianan kemungkinan adanya penyakit penyerta (Infeksi TB dll)",
       "Kunjungan ulang 30 hari"
     );
     res.push(gizi);
-  }
-  if(data.gizi.klasifikasiGizi.gizi_klasifikasi === "Gizi Normal"){
+  } else if (data.gizi.klasifikasiGizi.gizi_klasifikasi === "Gizi Normal") {
     gizi.push(
       "Jika anak berumur kurang dari 2 tahun, lakukan penilaian pemberian makan dan nasihati sesuai Anjuran Makan Untuk Anak Sehat Maupun Sakit. Bila ada masalah pemberian makan, kunjungan ulang 7 hari",
       "Anjurkan untuk menimbang berat badan anak setiap bulan"
@@ -128,13 +136,9 @@ const processTindakan = (data) => {
   }
 
   //ANEMIA
-  if(data.anemia.klasifikasiAnemia.anemia_klasifikasi === "Anemia Berat"){
-    anemia.push(
-      "Bila masih menyusu, teruskan pemberian ASI",
-      "RUJUK SEGERA"
-    )
-  }
-  if(data.anemia.klasifikasiAnemia.anemia_klasifikasi === "Anemia"){
+  if (data.anemia.klasifikasiAnemia.anemia_klasifikasi === "Anemia Berat") {
+    anemia.push("Bila masih menyusu, teruskan pemberian ASI", "RUJUK SEGERA");
+  } else if (data.anemia.klasifikasiAnemia.anemia_klasifikasi === "Anemia") {
     anemia.push(
       "Lakukan Penilaian Pemberian Makan pada anak. Bila ada masalah, beri konseling pemberian makan dan kunjungan ulang 7 hari",
       "Beri zat besi",
@@ -142,74 +146,39 @@ const processTindakan = (data) => {
       "Jika daerah Endemis Tinggi Malaria: periksa dan obati malaria terlebih dahulu jika positif",
       "Nasihati kapan kembali segera",
       "Kunjungan ulang 14 hari"
-    )
-  }
-  if(data.anemia.klasifikasiAnemia.anemia_klasifikasi === "Tidak Anemia"){
+    );
+  } else if (
+    data.anemia.klasifikasiAnemia.anemia_klasifikasi === "Tidak Anemia"
+  ) {
     anemia.push(
       "Jika anak < 2 tahun, nilai pemberian makanan pada anak. Jika ada masalah pemberian makanan, kunjungan ulang 7 hari"
-    )
+    );
   }
 
   // HIV
-  if(data.hiv.klasifikasiHIV.hiv_klasifikasi === "Infeksi HIV Terkonfirmasi"){
+  if (data.hiv.klasifikasiHIV.hiv_klasifikasi === "Infeksi HIV Terkonfirmasi") {
     hiv.push(
       "Rujuk ke puskesmas/RS Rujukan ARV untuk mendapatkan terapi ARV dan Kotrimoksasol profilaksis"
-    )
-  }
-  if(data.hiv.klasifikasiHIV.hiv_klasifikasi === "Diduga terinfeksi HIV"){
+    );
+  } else if (
+    data.hiv.klasifikasiHIV.hiv_klasifikasi === "Diduga terinfeksi HIV"
+  ) {
     hiv.push(
       "Rujuk ke puskesmas/RS Rujukan ARV untuk mendapatkan pemeriksaan lebih lanjut dan terapi ARV dan Kotrimoksasol profilaksis"
-    )
-  }
-  if(data.hiv.klasifikasiHIV.hiv_klasifikasi === "Terpajan HIV"){
+    );
+  } else if (data.hiv.klasifikasiHIV.hiv_klasifikasi === "Terpajan HIV") {
     hiv.push(
       "Rujuk ke puskesmas/RS rujukan ARV untuk mendapatkan pemeriksaan lebih lanjut dan ARV profilaksis serta Kotrimoksasol profilaksis"
-    )
-  }
-  if(data.hiv.klasifikasiHIV.hiv_klasifikasi === "Mungkin bukan infeksi HIV"){
-    hiv.push(
-      "Tangani Infeksi yang ada"
-    )
+    );
+  } else if (
+    data.hiv.klasifikasiHIV.hiv_klasifikasi === "Mungkin bukan infeksi HIV"
+  ) {
+    hiv.push("Tangani Infeksi yang ada");
   }
 
   return res;
 };
 
-//diare dehidrasi berat
-//Jika Tidak ada klasifikasi berat lain: Beri cairan untuk dehidrasi berat dan tablet Zinc sesuai rencana terapi C
-//Jika anak juga mempunyai klasifikasi berat lain:
-//- RUJUK SEGERA
-//- Jika masih bisa minum, berikan ASI dan larutan oralit selama perjalanan
-//Jika anak >2 tahun dan ada wabah kolera di daerah tersebut, beri antibiotik untuk kolera
-
-//diare dehidrasi ringan/sedang
-//Beri cairan, tablet Zinc dan makanan sesuai Rencana Terapi B
-//Jika terdapat klasifikasi berat lain :
-//- RUJUK SEGERA
-//- Jika masih bisa minum, berikan ASI dan larutan oralit selama perjalanan
-//Nasihati kapan kembali segera
-//Kunjungan ulang 3 hari jika tidak ada perbaikan
-
-//diare tanpa dehidrasi
-//Beri cairan, tablet Zinc dan makanan sesuai Rencana Terapi A
-//Nasihati kapan kembali segera.
-//Kunjungan ulang 3 hari jika tidak ada perbaikan.
-
-//diare persisten berat - jika diare 14 hari atau lebih
-//Atasi dehidrasi sebelum dirujuk, kecuali ada klasifikasi berat lain.
-//RUJUK
-
-//diare persisten - jika diare 14 hari atau lebih
-//Nasihati pemberian makan untuk Diare Persisten.
-//Beri tablet zinc selama 10 hari berturut-turut
-//Nasihati kapan kembali segera
-//Kunjungan ulang 3 hari
-
-//disentri - jika ada darah dalam tinja
-//Beri antibiotik yang sesuai
-//Beri tablet zinc selama 10 hari berturut-turut
-//Nasihati kapan kembali segera.
-//Kunjungan ulang 3 hari.
-module.exports = {
+export default {
   processTindakan,
 };

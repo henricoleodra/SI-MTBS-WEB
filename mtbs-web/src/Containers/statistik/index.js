fa7ba1b321a4864ea4fb2b72aea98cce2299bd80
Henrico Leodra <leodra28@gmail.com>
2020-11-06 01:18:53 +0700

    [UPDATE] Title

--- mtbs-web/src/Containers/statistik/index.js
+++ mtbs-web/src/Containers/statistik/index.js
@@ -1,11 +1,21 @@
 import React from "react";
-import { Row, Col, Label } from "reactstrap";
+import { Row, Col } from "reactstrap";
 import { Wrapper } from "./style";
 
 const Index = (props) => {
   return (
     <Wrapper>
-      <div className="statistik-wrapper">{/* Isi Disini */}</div>
+      <div className="statistik-wrapper">
+        <Row>
+          <Col>
+            <h4 className="px-2 pt-2 text-muted statistik-title font-weight-light">
+              Statistik
+            </h4>
+            <hr className="my-0" />
+          </Col>
+        </Row>
+        {/* Isi Disini */}
+      </div>
     </Wrapper>
   );
 };
--- mtbs-web/src/Containers/trend/index.js
+++ mtbs-web/src/Containers/trend/index.js
@@ -1,11 +1,21 @@
 import React from "react";
-import { Row, Col, Label } from "reactstrap";
+import { Row, Col } from "reactstrap";
 import { Wrapper } from "./style";
 
 const Index = (props) => {
   return (
     <Wrapper>
-      <div className="trend-wrapper">{/* Isi Disini */}</div>
+      <div className="trend-wrapper">
+        <Row>
+          <Col>
+            <h4 className="px-2 pt-2 text-muted trend-title font-weight-light">
+              Trend
+            </h4>
+            <hr className="my-0" />
+          </Col>
+        </Row>
+        {/* Isi Disini */}
+      </div>
     </Wrapper>
   );
 };

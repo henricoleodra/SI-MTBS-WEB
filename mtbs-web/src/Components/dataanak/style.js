9090d4a3370c6c066926e948195f1f6a916a56b9
Henrico Leodra <leodra28@gmail.com>
2020-11-29 18:46:32 +0700

    [ADD] Hover Effect

--- mtbs-web/src/Components/dataanak/style.js
+++ mtbs-web/src/Components/dataanak/style.js
@@ -1,9 +1,14 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-export const Wrapper = styled.div `
-    .wrapper-data-anak {
-        border-color : #46d0fe;
-        border-width : 3px;
-        border-radius: 25px;
-    }
+export const Wrapper = styled.div`
+  .wrapper-data-anak {
+    border-color: #46d0fe;
+    border-width: 3px;
+    border-radius: 25px;
+  }
+
+  .wrapper-data-anak:hover {
+    cursor: pointer;
+    background-color: #e6e9eb;
+  }
 `;

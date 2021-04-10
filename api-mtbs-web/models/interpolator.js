ceb27a7429cf357aa64d195be51e022c5cfbbb66
Henrico Leodra <leodra28@gmail.com>
2021-04-10 20:58:28 +0700

    [UPDATE] Interpolator Point

--- api-mtbs-web/models/interpolator.js
+++ api-mtbs-web/models/interpolator.js
@@ -1,25 +1,25 @@
 module.exports = class Interpolator {
-        constructor(points, tinggiBadan) {
-            this.points = points;
-            this.tinggiBadan = tinggiBadan;
-        }
+  constructor(points, tinggiBadan) {
+    this.points = points;
+    this.tinggiBadan = tinggiBadan;
+  }
 
-        interpolateCalculate() {
-            let x1 = 0;
-            let x2 = 0;
-            let y1 = 0;
-            let y2 = 0;
+  interpolateCalculate() {
+    let x1 = 0;
+    let x2 = 0;
+    let y1 = 0;
+    let y2 = 0;
 
-            for (let i = 0; i < this.points.length; i++) {
-                if (this.tinggiBadan <= this.points[i].x) {
-                    x1 = this.points[i - 1].x;
-                    y1 = this.points[i - 1].y;
-                    x2 = this.points[i].x;
-                    y2 = this.points[i].y;
-                    break;
-                }    
-            }
+    for (let i = 0; i < this.points.length; i++) {
+      if (this.tinggiBadan <= this.points[i].x) {
+        x1 = this.points[i - 1].x;
+        y1 = this.points[i - 1].y;
+        x2 = this.points[i].x;
+        y2 = this.points[i].y;
+        break;
+      }
+    }
 
-            return y1 + (y2 - y1) * ((this.tinggiBadan - x1) / (x2 - x1));
-        }
-}
+    return y1 + (y2 - y1) * ((this.tinggiBadan - x1) / (x2 - x1));
+  }
+};
